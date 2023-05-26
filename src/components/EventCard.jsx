02dd8fa4ec8dccac820ddda1a1b32d1b3d22bae1
Michelle <chenmic8@msu.edu>
2023-05-26 00:32:23 -0400

    pre mapbox and event week view done

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -32,6 +32,17 @@ const EventCard = ({ event }) => {
   //   e.preventDefault();
   //   console.log("CLICKED!THIS IS THIS EVENT'S ID", event);
   // };
+  const formatAMPM = (date) => {
+    date = new Date(date);
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let ampm = hours >= 12 ? "pm" : "am";
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    let strTime = hours + ":" + minutes + ampm;
+    return strTime;
+  };
 
   return (
     <>
@@ -42,7 +53,11 @@ const EventCard = ({ event }) => {
               {event.title}
             </Typography>
             <Typography sx={{ mb: 1.5 }} color='text.secondary'>
-              {event.beginTime} - {event.endTime}
+              {formatAMPM(event.beginTime)} - {formatAMPM(event.endTime)}
+            </Typography>
+            <Typography>{event.driver.firstName}</Typography>
+            <Typography>
+              {event.car.make} {event.car.model}
             </Typography>
             {event.endLocation && (
               <Typography variant='body2'>{event.endLocation.name}</Typography>
@@ -57,7 +72,7 @@ const EventCard = ({ event }) => {
         aria-describedby='modal-modal-description'
       >
         <Box sx={style}>
-          <EditEventModal event={event} handleClose={handleClose}/>
+          <EditEventModal event={event} handleClose={handleClose} />
         </Box>
       </Modal>
     </>
--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,7 +1,7 @@
-import { Box, Typography, Button, Modal } from "@mui/material";
+import { Box, Typography, Button, Modal, Grid } from "@mui/material";
 
 import EventCard from "../components/EventCard";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { LoadingContext } from "../context/loadingContext";
 import AddEvent from "../components/AddEventModal";
 
@@ -22,13 +22,104 @@ const style = {
 const Events = () => {
   const { dataIsLoading, familyEvents } = useContext(LoadingContext);
 
+  const [orderedDates, setOrderedDates] = useState([]);
+
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
+  //GET THE DAYS OF THIS WEEK
+
+  //MAP THROUGH THE EVENTS AND ASSIGN THEM TO SUN-SAT ARRAYS
+
+  const datesAreOnSameDay = (first, second) =>
+    first.getFullYear() === second.getFullYear() &&
+    first.getMonth() === second.getMonth() &&
+    first.getDate() === second.getDate();
+
+  const sortByTime = (data) => {
+    return data.sort(function (a, b) {
+      return a.beginTime.localeCompare(b.beginTime);
+    });
+  };
+
+  const dates = (current) => {
+    var week = new Array();
+    // Starting Sunday
+    current.setDate(current.getDate() - current.getDay());
+    for (var i = 0; i < 7; i++) {
+      week.push(new Date(current));
+      current.setDate(current.getDate() + 1);
+    }
+    return week;
+  };
+
+  // console.log(data)
+
+  // console.log(datesAreOnSameDay(new Date("2023-05-25T15:11:00.000Z"),new Date("2023-05-25T15:11:00.000Z")))
+  const [sunday, setSunday] = useState([]);
+  const [monday, setMonday] = useState([]);
+  const [tuesday, setTuesday] = useState([]);
+  const [wednesday, setWednesday] = useState([]);
+  const [thursday, setThursday] = useState([]);
+  const [friday, setFriday] = useState([]);
+  const [saturday, setSaturday] = useState([]);
+  let Sunday = [];
+  let Monday = [];
+  let Tuesday = [];
+  let Wednesday = [];
+  let Thursday = [];
+  let Friday = [];
+  let Saturday = [];
+  useEffect(() => {
+    console.log(dates(new Date()));
+    const thisWeek = dates(new Date());
+    familyEvents.map((event) => {
+      let eventDate = new Date(event.beginTime);
+      if (datesAreOnSameDay(thisWeek[0], eventDate)) Sunday.push(event);
+      else if (datesAreOnSameDay(thisWeek[1], eventDate)) Monday.push(event);
+      else if (datesAreOnSameDay(thisWeek[2], eventDate)) Tuesday.push(event);
+      else if (datesAreOnSameDay(thisWeek[3], eventDate)) Wednesday.push(event);
+      else if (datesAreOnSameDay(thisWeek[4], eventDate)) Thursday.push(event);
+      else if (datesAreOnSameDay(thisWeek[5], eventDate)) Friday.push(event);
+      else if (datesAreOnSameDay(thisWeek[6], eventDate)) Saturday.push(event);
+    });
+    console.log(
+      "Sunday",
+      Sunday,
+      "\n\n\nMonday",
+      Monday,
+      "\n\n\nTuesday",
+      Tuesday,
+      "\n\n\nThursday",
+      Thursday,
+      "\n\n\nFriday",
+      Friday,
+      "\n\n\nSaturday",
+      Saturday
+    );
+
+    //SORT BY TIME
+    Sunday = sortByTime(Sunday);
+    Monday = sortByTime(Monday);
+    Tuesday = sortByTime(Tuesday);
+    Wednesday = sortByTime(Wednesday);
+    Thursday = sortByTime(Thursday);
+    Friday = sortByTime(Friday);
+    Saturday = sortByTime(Saturday);
+
+    setSunday(Sunday);
+    setMonday(Monday);
+    setTuesday(Tuesday);
+    setWednesday(Wednesday);
+    setThursday(Thursday);
+    setFriday(Friday);
+    setSaturday(Saturday);
+  }, [familyEvents]);
+
   return (
     <>
-      {!dataIsLoading ? (
+      {/* {!dataIsLoading ? (
         <>
           <Button onClick={handleOpen}>Add Event</Button>
           <Typography>Show week</Typography>
@@ -51,39 +142,65 @@ const Events = () => {
         </>
       ) : (
         <p>LOADING...</p>
+      )} */}
+      {/* //FOR SEVEN COLUMNS, ONE FOR EACH DAY OF THE WEEK!!!!!!!!!!!!!!!!!!!!!!! */}
+      {!dataIsLoading && familyEvents ? (
+        <>
+          <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} md={12 / 7}>
+                {/* <Item>Sunday</Item> */}
+                <Box>
+                  <Typography>Sunday</Typography>
+                  {sunday.map((event) => {
+                    return <EventCard key={event._id} event={event} />;
+                  })}
+                  {/* <EventCard event={familyEvents} /> */}
+                </Box>
+              </Grid>
+              <Grid item xs={12} md={12 / 7}>
+                <Typography>Monday</Typography>
+                {monday.map((event) => {
+                  return <EventCard key={event._id} event={event} />;
+                })}
+              </Grid>
+              <Grid item xs={12} md={12 / 7}>
+                <Typography>Tuesday</Typography>
+                {tuesday.map((event) => {
+                  return <EventCard key={event._id} event={event} />;
+                })}
+              </Grid>
+              <Grid item xs={12} md={12 / 7}>
+                <Typography>Wednesday</Typography>
+                {wednesday.map((event) => {
+                  return <EventCard key={event._id} event={event} />;
+                })}
+              </Grid>
+              <Grid item xs={12} md={12 / 7}>
+                <Typography>Thursday</Typography>
+                {thursday.map((event) => {
+                  return <EventCard key={event._id} event={event} />;
+                })}
+              </Grid>
+              <Grid item xs={12} md={12 / 7}>
+                <Typography>Friday</Typography>
+                {friday.map((event) => {
+                  return <EventCard key={event._id} event={event} />;
+                })}
+              </Grid>
+              <Grid item xs={12} md={12 / 7}>
+                <Typography>Saturday</Typography>
+                {saturday.map((event) => {
+                  return <EventCard key={event._id} event={event} />;
+                })}
+              </Grid>
+            </Grid>
+          </Box>
+        </>
+      ) : (
+        <p>Loading...</p>
       )}
     </>
-
-    //FOR SEVEN COLUMNS, ONE FOR EACH DAY OF THE WEEK!!!!!!!!!!!!!!!!!!!!!!!
-    // <Box sx={{ flexGrow: 1 }}>
-    //   <Grid container spacing={2}>
-    //     <Grid item xs={12} md={12 / 7}>
-    //       {/* <Item>Sunday</Item> */}
-    //       <Box sx={{ height: "100%", background: "red" }}>
-    //         <Typography>Sunday</Typography>
-    //         <EventCard />
-    //       </Box>
-    //     </Grid>
-    //     <Grid item xs={12} md={12 / 7}>
-    //       <Typography>Monday</Typography>
-    //     </Grid>
-    //     <Grid item xs={12} md={12 / 7}>
-    //       <Typography>Tuesday</Typography>
-    //     </Grid>
-    //     <Grid item xs={12} md={12 / 7}>
-    //       <Typography>Wednesday</Typography>
-    //     </Grid>
-    //     <Grid item xs={12} md={12 / 7}>
-    //       <Typography>Thursday</Typography>
-    //     </Grid>
-    //     <Grid item xs={12} md={12 / 7}>
-    //       <Typography>Friday</Typography>
-    //     </Grid>
-    //     <Grid item xs={12} md={12 / 7}>
-    //       <Typography>Saturday</Typography>
-    //     </Grid>
-    //   </Grid>
-    // </Box>
   );
 };
 
