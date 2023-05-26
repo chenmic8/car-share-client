47db857bb9e93c70ec81152a75ab41dc62373723
Michelle <chenmic8@msu.edu>
2023-05-26 09:15:50 -0400

    deployed backend url

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -39,7 +39,7 @@ const AuthProvider = ({ children }) => {
     onSuccess: async ({ code }) => {
       console.log("TRYING TO GET THE RESPONSE OFTHE GOOGLE TTHING")
       try {
-        const tokens = await axios.post("http://localhost:8080/auth/google", {
+        const tokens = await axios.post("https://family-car-share.fly.dev/auth/google", {
           // http://localhost:4000/auth/google backend that will exchange the code
           code,
         });
